36c7fc8bb5f9196485c834c4df4f82dfbcc61058
= <morganobala@gmail.com>
2018-05-19 02:49:32 -0700

    first commmit

---
+++ data_structures.py
@@ -0,0 +1,20 @@
+#lists
+name_list = ['Micky', 'Carol', 'Ben', 'Morgan', 'Mike', 'Loyce', 'Lucia']
+
+#sets, 
+names_set = {'Micky', 'Carol', 'Ben', 'Morgan', 'Mike', 'Loyce', 'Lucia'}
+'Micky', 'Carol', 'Ben', 'Morgan', 'Mike', 'Loyce', 'Lucia'
+#tuples
+
+names_tuple = ('Micky', 'Carol', 'Ben', 'Morgan', 'Mike', 'Loyce', 'Lucia')
+
+
+#dictionary
+names_dict = {'name':'Micky','name2': 'Carol', 'name3':'Ben', 'name4':'Morgan', 'name5':'Mike', 'name6':'Loyce', 'name6':'Lucia'}
+ 
+ # key:values[4
+ # ]
+
+
+another_dict = {'names': ['Micky', 'Carol', 'Ben', 'Morgan', 'Mike', 'Loyce', 'Lucia']}
+
